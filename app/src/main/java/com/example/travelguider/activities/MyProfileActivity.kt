d33bd1a88d492af9144c511cd2328731f008a530
Kartik-Rao11 <kartik.rao29@gmail.com>
2022-07-08 07:49:52 +0530

    display user data in the UI section completed

--- app/src/main/java/com/example/travelguider/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/travelguider/activities/MyProfileActivity.kt
@@ -2,7 +2,10 @@ package com.example.travelguider.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.travelguider.R
+import com.example.travelguider.firebase.FirestoreCLass
+import com.example.travelguider.models.User
 import kotlinx.android.synthetic.main.activity_my_profile.*
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
@@ -11,6 +14,8 @@ class MyProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
         setupActionBar()
+
+        FirestoreCLass().signInUser(this)
     }
     private fun setupActionBar(){
         setSupportActionBar(toolbar_my_profile_activity)
@@ -24,5 +29,23 @@ class MyProfileActivity : AppCompatActivity() {
 
         }
         toolbar_my_profile_activity.setNavigationOnClickListener{onBackPressed()}
+    }
+
+    //display user information in my profile section
+    fun setUserDataInUI(user: User){
+        //image
+        Glide
+            .with(this@MyProfileActivity)
+            .load(user.image)
+            .centerCrop()
+            .placeholder(R.drawable.user_image)
+            .into(iv_profile_user_image)
+
+            //name email and mobile no.
+            et_name.setText(user.name)
+            et_email.setText(user.email)
+            if(user.mobile!=0L){
+                et_mobile.setText(user.mobile.toString())
+            }
     }
 }
